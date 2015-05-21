acc82e8ce044049b6d4b627541ac10499f4ff5a1
vjpratap <vijayprsingh619@gmail.com>
2015-05-21 15:13:26 +0530

    Ui is ready for Tic Tac Tow screen

---
+++ Tic-Tac-Tow/TicTacTowViewController.h
@@ -0,0 +1,13 @@
+//
+//  TicTacTowViewController.h
+//  Tic-Tac-Tow
+//
+//  Created by Vijay Pratap Singh on 21/05/15.
+//  Copyright (c) 2015 thoughtworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TicTacTowViewController : UIViewController
+
+@end
---
+++ Tic-Tac-Tow/TicTacTowViewController.m
@@ -0,0 +1,44 @@
+//
+//  TicTacTowViewController.m
+//  Tic-Tac-Tow
+//
+//  Created by Vijay Pratap Singh on 21/05/15.
+//  Copyright (c) 2015 thoughtworks. All rights reserved.
+//
+
+#import "TicTacTowViewController.h"
+
+@interface TicTacTowViewController ()
+
+@end
+
+@implementation TicTacTowViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+//UIView *lineView = [[UIView alloc] initWithFrame:CGRectMake(0, 200, self.view.bounds.size.width, 1)];
+//lineView.backgroundColor = [UIColor blackColor];
+//[self.view addSubview:lineView];
+//[lineView release];
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
