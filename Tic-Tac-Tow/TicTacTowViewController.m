1ff8424fe8b7fce571334a562fb0dab15d29cf27
vjpratap <vijayprsingh619@gmail.com>
2015-05-25 09:49:10 +0530

    user is able to see the win massage if the column is correctly field

--- Tic-Tac-Tow/TicTacTowViewController.m
+++ Tic-Tac-Tow/TicTacTowViewController.m
@@ -30,15 +30,45 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-- (IBAction)buttonTabbed:(id)sender {
-    NSArray *mark = @[@"X",@"O"];
+
+-(BOOL)checkColumn:(NSString*)playerMark{
+    for(int row = 0; row < 3; row++){
+        BOOL won = YES;
+        for (int col = 0; col < 3; col++) {
+            won = won && ([self.fillMark[row * 3 + col] isEqualToString:playerMark]);
+        }
+        if (won)
+            return YES;
+    }
+    return NO;
+}
+
+-(BOOL)isWin :(NSString*)playerMark{
+    return [self checkColumn : playerMark];
+}
+
+- (void) afterTabbingActions: (NSArray *)mark{
     NSString *player = @"Player . term";
-    [sender setTitle:mark[self.count%2] forState:UIControlStateNormal];
-    [self.fillMark replaceObjectAtIndex:[sender tag] withObject:mark[self.count%2]];
-    [sender setEnabled:NO];
     self.count++;
     [self.showNameOfUser setText:[player stringByReplacingOccurrencesOfString:@"." withString:mark[self.count%2]]];
+}
+
+- (IBAction)buttonTabbed:(id)sender {
+    NSArray *mark = @[@"X",@"O"];
+    [sender setTitle:mark[self.count%2] forState:UIControlStateNormal];
+    [sender setEnabled:NO];
+    [self.fillMark replaceObjectAtIndex:[sender tag] withObject:mark[self.count%2]];
+    if ([self isWin:mark[self.count%2]]) {
+        UIAlertView *toast = [[UIAlertView alloc] initWithTitle:nil message:@"You Won" delegate:nil cancelButtonTitle:nil otherButtonTitles:nil, nil];
+        [toast show];
+        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+            [toast dismissWithClickedButtonIndex:0 animated:YES];
+        });
+        
+    }
     
+    [self afterTabbingActions : mark];
 }
 
 /*
